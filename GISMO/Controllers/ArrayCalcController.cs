ad229ba592b704a89d35b6d977cab826377a95c8
Andrei Francis Hulog <andrei.hulog@gmail.com>
2023-01-06 14:50:28 +0800

    test

---
+++ GISMO/Controllers/ArrayCalcController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GISMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArrayCalcController : ControllerBase
+    {
+        [HttpGet("reverse")]
+        public IActionResult Reverse()
+        {
+            string result = string.Empty;
+            result = HttpContext.Request.Query["productids"];
+            string[] productIdArray = result.Split(',');
+
+            List<string> results = new List<string>();
+            for (int counter = productIdArray.Length; counter > 0; counter--)
+            {
+                results.Add(productIdArray[counter - 1]);
+            }
+            return Ok(results);
+        }
+
+        [HttpGet("deletepart")]
+        public IActionResult Delete()
+        {
+            string result = string.Empty;
+            return Ok(result);
+        }
+    }
+}
