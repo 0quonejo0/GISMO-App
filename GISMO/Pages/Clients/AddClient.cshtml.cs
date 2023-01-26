039d51c2507f73743c23ba7c00ca38969d1c46bc
Randy Oliveros Ramos <randy.o.ramos@outlook.com>
2023-01-27 01:49:00 +0800

    GB 27.Jan @ 1:48AM

--- GISMO/Pages/Clients/AddClient.cshtml.cs
+++ GISMO/Pages/Clients/AddClient.cshtml.cs
@@ -1,12 +1,43 @@
+using GISMO.Data;
+using GISMO.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace GISMO.Pages.Clients
 {
     public class AddClientModel : PageModel
     {
+        private readonly AppDbContext _dbcontext;
+        [BindProperty]
+        [Required(ErrorMessage = "Client Name required")]
+        public string ClientName { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Type of Client is required")]
+        public string ClientAddress { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Contacts required")]
+        public string ClientContacts { get; set; }
+        [BindProperty]
+        public string ClientAppLic { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Client type required")]
+        public string ClientType { get; set; }
+
         public void OnGet()
         {
         }
+        public async void OnPost()
+        {
+            Client client = new Client();
+            client.ClientName = ClientName;
+            client.Type = ClientType;
+            client.Address = ClientAddress;
+            client.Contacts = ClientContacts;
+            client.AppLicense = ClientAppLic;
+            await _dbcontext.Clientele.AddAsync(client);
+            _dbcontext.SaveChanges();
+        }
+
     }
 }
