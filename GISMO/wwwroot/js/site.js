bfe90fa4361611083a9cea774ddeb9b3dd312f71
0quonejo0 <76120015+0quonejo0@users.noreply.github.com>
2023-02-08 10:42:16 +0800

    Pred 8.Feb.2023 @ 10:42AM

--- GISMO/wwwroot/js/site.js
+++ GISMO/wwwroot/js/site.js
@@ -3,13 +3,13 @@
 
 // Write your JavaScript code.
 
+// TOP MAIN MENU
 function loadHtml(id, filename) {
-    console.log(`div id: ${id}, filename: ${filename}`);
+    //console.log(`div id: ${id}, filename: ${filename}`);
 
     let xhttp;
     let element = document.getElementById(id);
     let file = filename;
-    console.log('here1');
     if (file) {
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
@@ -18,10 +18,43 @@ function loadHtml(id, filename) {
                 if (this.status == 404) { element.innerHTML = "<h1> Page not found</h1>"; }
             }
         }
-        console.log('here2');
         xhttp.open("GET", `${file}`, true);
         xhttp.send();
         console.log(file);
         return;
     }
+}
+
+// VESSEL DETAILS PAGE, TAB PANELS
+function openThis(evt, tabCont) {
+    // Declare all variables
+    var i, tabContentFormat, tablinks;
+
+    // Get all elements with class="tabContentFormat" and hide them
+    tabContentFormat = document.getElementsByClassName("tabContentFormat");
+    for (i = 0; i < tabContentFormat.length; i++) {
+        tabContentFormat[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabCont).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+//document.getElementById("defaultOpen").click();
+
+// IMAGE ENLARGE
+// Get the img object using its Id
+img = document.getElementById("eventimage");
+// Function to increase image size
+function enlargeImg() {
+    // Set image size to 1.5 times original
+    img.style.transform = "scale(1.5)";
+    // Animation effect
+    img.style.transition = "transform 0.25s ease";
 }
